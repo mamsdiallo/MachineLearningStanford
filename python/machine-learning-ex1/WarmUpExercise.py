2655348db793e683ccd60833e15c3e2ecf9ea814
Mamadou Diallo <mams.diallo@gmail.com>
2017-01-22 09:34:28 +0100

    implement warmupeexercise in python

---
+++ python/machine-learning-ex1/WarmUpExercise.py
@@ -0,0 +1,12 @@
+# WARMUPEXERCISE Example function
+# A = WARMUPEXERCISE() is an example function 
+# that returns the 5x5 identity matrix
+# function A = warmUpExercise()
+
+#for numerical computaions we can use numpy library
+import numpy as np
+
+def warmUpExercise():
+    A = np.identity(3)
+    return A
+    
---
+++ python/machine-learning-ex1/ex1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 22 2017
+
+@author: Diallo
+"""
+# We can use the pandas library in python to read in the csv file.
+import pandas as pd
+#for numerical computaions we can use numpy library
+import numpy as np
+
+print "Running warmUpExercise"
